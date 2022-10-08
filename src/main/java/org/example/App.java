21ed43ebb4482cd22ef342b15f6d51334cdd7d7a
AramaJava <abr-aram@yandex.ru>
2022-10-08 17:02:52 +0500

    божественные домашки вер2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,15 +4,21 @@ package org.example;
 
 public class App {
 
-    public static void main(String[] args) throws IncorrectCatWeightException {
+    public static void main(String[] args) {
         //System.out.printf("\nВозраст в днях: %d", getAge(1896, 10, 20));
         //System.out.printf("\nСлед. днюха через %s дней будет: %s",returnRound(getAge(1896, 10, 20),1000), nextBirthday(1896,10,20));
         // Barsik.setWeight(-10);
 
-        Cat murzik = CatFactory.createCat("Murzik", 10);
-        Cat barsik = CatFactory.createCat("Barsik", -3);
+        try {
+            Cat murzik = CatFactory.createCat("Murzik", 10);
+            Cat barsik = CatFactory.createCat("Barsik", -3);
+            System.out.printf("\nКот %s - %s", murzik.getName(), (murzik.isAngry() ? "злой" : "добрый"));
+            System.out.printf("\nКот %s - %s", barsik.getName(), (barsik.isAngry() ? "злой" : "добрый"));
 
-        System.out.printf("\nКот %s - %s", murzik.getName(), (murzik.isAngry() ? "злой" : "добрый"));
-        System.out.printf("\nКот %s - %s", barsik.getName(), (barsik.isAngry() ? "злой" : "добрый"));
+        } catch (IncorrectCatWeightException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
