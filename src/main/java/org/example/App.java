bfff9a1763c2862153212d14a77c76f4e04c01e9
AramaJava <abr-aram@yandex.ru>
2022-10-08 17:04:26 +0500

    божественные домашки вер2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ package org.example;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IncorrectCatWeightException {
         //System.out.printf("\nВозраст в днях: %d", getAge(1896, 10, 20));
         //System.out.printf("\nСлед. днюха через %s дней будет: %s",returnRound(getAge(1896, 10, 20),1000), nextBirthday(1896,10,20));
         // Barsik.setWeight(-10);
@@ -16,7 +16,7 @@ public class App {
             System.out.printf("\nКот %s - %s", barsik.getName(), (barsik.isAngry() ? "злой" : "добрый"));
 
         } catch (IncorrectCatWeightException e) {
-            System.out.println(e.getMessage());
+            throw new IncorrectCatWeightException("Неправильно введен вес!");
         }
 
 
